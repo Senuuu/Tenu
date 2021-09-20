0bb87565f05e7b5e8d62e09e7af93f66640251cd
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-20 13:54:01 +0530

    Update zxdocu.js

--- plugins/zxdocu.js
+++ plugins/zxdocu.js
@@ -24,19 +24,19 @@ await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), Messa
     }));
 
 Asena.addCommand({pattern: 'kunigif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/kuni?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/kuni?apikey=aca83a4354ac`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
 
     }));
 
 Asena.addCommand({pattern: 'analgif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/anal?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/anal?apikey=aca83a4354ac`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
 
     }));
 
 Asena.addCommand({pattern: 'spankgif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/spank?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/spank?apikey=aca83a4354ac`, { responseType: 'arraybuffer' })
 await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
 
     }));
