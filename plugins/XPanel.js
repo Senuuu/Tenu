28eb7bc7ce2472c78b8860b4ff2233304271c70f
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-20 14:48:06 +0530

    Create XPanel.js

---
+++ plugins/XPanel.js
@@ -0,0 +1,37 @@
+ /* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+Wa.me/+94766598862
+*/
+
+const Neotro = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+var ggg = Buffer.from(clh.cd, 'base64')
+var ddd = ggg.toString('utf-8')
+
+
+Neotro.addCommand({pattern: 'xpanel', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    const rows = [
+        {title: 'Row 1', description: "Hello it's description 1", rowId:"rowid1"},
+        {title: 'Row 2', description: "Hello it's description 2", rowId:"rowid2"}
+       ]
+       
+       const sections = [{title: "Section 1", rows: rows}]
+       
+       const button = {
+        buttonText: 'Click Me!',
+        description: "Hello it's list message",
+        sections: sections,
+        listType: 1
+       }
+       
+       await message.client.sendMessage(message.jid, button, MessageType.listMessage)
+    
+    }));
