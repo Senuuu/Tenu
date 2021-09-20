7f1317d87a60776b4dc5936e3b05fe5b5ee45fe7
Teenuh-X <88507137+jesonpro@users.noreply.github.com>
2021-09-20 13:40:29 +0530

    Create zxdocu.js

---
+++ plugins/zxdocu.js
@@ -0,0 +1,42 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+Wa.me/+94766598862
+*/
+
+const Asena = require('../events');
+const Config = require('../config');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+
+Asena.addCommand({pattern: 'feetggif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/feetg?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
+
+    }));
+
+Asena.addCommand({pattern: 'pussygif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/pussy?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
+
+    }));
+
+Asena.addCommand({pattern: 'kunigif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/kuni?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
+
+    }));
+
+Asena.addCommand({pattern: 'analgif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/anal?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
+
+    }));
+
+Asena.addCommand({pattern: 'spankgif ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+var respoimage = await axios.get(`https://${Config.NEOS}.${Config.NEOY}/api/anime/nsfw/spank?apikey=${Config.NEOA}`, { responseType: 'arraybuffer' })
+await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.document, {mimetype: Mimetype.gif, caption: '*🎭ɴᴇᴏᴛʀᴏ𝙭🪐*'})
+
+    }));
